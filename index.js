38b5eee757b6ee2e1ddab9555694395a98b9bbe2
Arianne Dee <ariannedee@gmail.com>
2018-04-09 10:34:47 -0700

    Update to personal website

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
+var show = function(id) {
+  $("section").not('#' + id).hide();
+  $("section#" + id).show();
+}
+
 $(document).ready(function(){
-    $("a.courses").click(function(e){
+    $("a.menu").click(function(e){
       e.preventDefault();
-      $("a.filter").removeClass("active");
-      $(this).addClass("active");
-      $(".item").not('.item.course').slideUp();
-      $(".item.course").slideDown();
       return false;
     });
     
-    $("a.projects").click(function(e){
-      e.preventDefault();
-      $("a.filter").removeClass("active");
-      $(this).addClass("active");
-      $(".item").not('.item.project').slideUp();
-      $(".item.project").slideDown();
-      return false;
-    });
-    
-    $("a.all").click(function(e){
-      e.preventDefault();
-      $("a.filter").removeClass("active");
-      $(this).addClass("active");
-      $(".item").slideDown();
-      return false;
-    });
+    show("about");
 });
